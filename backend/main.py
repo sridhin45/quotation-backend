97cecf823c445b813f23952f98fa5c2bb7ec5d36
sridhinm <sridhinmarar1@gmail.com>
2025-12-19 23:07:34 +0530

    CORS fix

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:4200",
         "http://127.0.0.1:4200",
-        "https://quotation-frontend-g74l.onrender.com",   # YOUR FRONTEND
+        "https://quotation-frontend-g74l.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
